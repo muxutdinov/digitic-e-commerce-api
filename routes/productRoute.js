599cbf117dfe994ac1ccf60533bef96905238bee
muxutdinov <jmuxutdinov@mail.ru>
2024-01-01 16:34:22 +0500

    first commit

---
+++ routes/productRoute.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const {
+  createProduct,
+  getaProduct,
+  getAllProduct,
+  updateProduct,
+  deleteProduct,
+  addToWishlist,
+  rating,
+} = require("../controller/productCtrl");
+const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
+const {uploadPhoto,productImgResize} = require('../middlewares/uploadImage')
+const router = express.Router();
+
+router.post("/", authMiddleware, isAdmin, createProduct);
+router.put("upload/:id", authMiddleware, isAdmin, uploadPhoto.array('images', 10) , productImgResize)
+router.get("/:id", getaProduct);
+router.put("/wishlist", authMiddleware, addToWishlist);
+router.put("/rating", authMiddleware, rating);
+
+router.put("/:id", authMiddleware, isAdmin, updateProduct);
+router.delete("/:id", authMiddleware, isAdmin, deleteProduct);
+
+router.get("/", getAllProduct);
+
+module.exports = router;
